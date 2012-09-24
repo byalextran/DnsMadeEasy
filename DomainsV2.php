9d0835d65e69a77ba415ffe2f30edb71e5826a51
David Baker <dbaker@acorncomputersolutions.com>
2012-09-24 16:36:14 -0600

    Partial support for the V2.0 API from DnsMadeEasy
    Added in fully working code for adjusting Records, but haven't
    done the work on the Domains or Subrecords.

---
+++ DomainsV2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Provides access to domain-related API calls.
+ *
+ * @package DnsMadeEasy
+ */
+class DnsMadeEasy_DomainsV2 extends DnsMadeEasy_BaseV2
+{
+	const API_URL = 'dns/managed';
+
+    // none of the functions for this are currently setup
+    // please fork me and add them!  Thanks!
+
+}
+?>
